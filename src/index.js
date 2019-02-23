95778f8b1c99b7f52bd7068f61f4e1025e2c6457
Gideon <GideonHarmse0404@gmail.com>
2019-02-23 20:19:11 +0200

    Confuring eslint

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 import Button from '@material-ui/core/Button';
 
 import './assets/styles/css/material-dashboard-react.css';
 
-const Index = () => {
-  return   <Button variant="contained" color="primary">
-  Hello World
-</Button>
-};
+const Index = () => (
+  <Button variant="contained" color="primary">
+    Hello World
+  </Button>
+);
 
-ReactDOM.render(<Index />, document.getElementById("index"));
+ReactDOM.render(<Index />, document.getElementById('index'));
