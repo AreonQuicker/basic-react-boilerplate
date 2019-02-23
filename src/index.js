521d357f2a6ba72e545ab09c02be6ae16cc9ba50
Gideon <GideonHarmse0404@gmail.com>
2019-02-23 20:09:56 +0200

    Setting up project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Button from '@material-ui/core/Button';
+
+import './assets/styles/css/material-dashboard-react.css';
 
 const Index = () => {
-  return <div>Hello React</div>;
+  return   <Button variant="contained" color="primary">
+  Hello World
+</Button>
 };
 
 ReactDOM.render(<Index />, document.getElementById("index"));
